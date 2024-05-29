c965017e1ed34f60b6e06a545b80b7a271c927eb
JoseyMedley <65869488+JoseyMedley@users.noreply.github.com>
2024-05-29 00:16:51 -0500

    fixing dependencies

--- install.sh
+++ install.sh
@@ -181,7 +181,7 @@
     echo
     sleep 3
 
-    sudo apt -y install libgmp3-dev libmysqlclient-dev libcurl4-gnutls-dev libkrb5-dev libldap2-dev libidn11-dev gnutls-dev \
+    sudo apt -y install libgmp3-dev default-libmysqlclient-dev libcurl4-gnutls-dev libkrb5-dev libldap2-dev libidn11-dev libgnutls28-dev \
     librtmp-dev sendmail mutt screen git
     sudo apt -y install pwgen -y
     echo -e "$GREEN Done...$COL_RESET"
@@ -197,9 +197,9 @@
 
     sudo apt -y install software-properties-common build-essential
     sudo apt -y install libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils git cmake libboost-all-dev zlib1g-dev libz-dev libseccomp-dev libcap-dev libminiupnpc-dev gettext
-    sudo apt -y install libminiupnpc10 libzmq5
+    sudo apt -y install libminiupnpc-dev libzmq5
     sudo apt -y install libcanberra-gtk-module libqrencode-dev libzmq3-dev
-    sudo apt -y install libqt5gui5 libqt5core5a libqt5webkit5-dev libqt5us5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
+    sudo apt -y install libqt5gui5 libqt5core5a libqt5webkit5-dev libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
     sudo add-apt-repository -y ppa:bitcoin/bitcoin
     sudo apt -y update  sudo apt install libdb5.3++ 
     sudo apt -y install libdb5.3 libdb5.3++ libdb5.3++-dev
