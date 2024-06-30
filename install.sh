119224f8433d624eba532cc6d125e255ffa9ba50
JoseyMedley <65869488+JoseyMedley@users.noreply.github.com>
2024-06-30 16:47:18 -0500

    git is already installed

--- install.sh
+++ install.sh
@@ -195,7 +195,7 @@ echo
 sleep 3
 
 sudo apt -y install software-properties-common build-essential
-sudo apt -y install libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils git cmake libboost-all-dev zlib1g-dev libz-dev libseccomp-dev libcap-dev libminiupnpc-dev gettext
+sudo apt -y install libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils cmake libboost-all-dev zlib1g-dev libz-dev libseccomp-dev libcap-dev libminiupnpc-dev gettext
 sudo apt -y install libminiupnpc-dev libzmq5 libzstd-dev libbrotli-dev libssh2-1-dev
 sudo apt -y install libcanberra-gtk-module libqrencode-dev libzmq3-dev
 sudo apt -y install libqt5gui5 libqt5core5a libqt5webkit5-dev libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
