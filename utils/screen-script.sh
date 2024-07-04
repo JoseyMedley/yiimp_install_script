b76fb607fbb0a6a33ee6759f26a87d843e13c573
JoseyMedley <65869488+JoseyMedley@users.noreply.github.com>
2024-07-04 13:28:07 -0500

    make script actually work on Debian

--- utils/screen-script.sh
+++ utils/screen-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-screen -dm -S main /var/web/main.sh
-screen -dm -S loop2 /var/web/loop2.sh
-screen -dm -S blocks /var/web/blocks.sh
-screen -dm -S debug tail -f /var/log/yiimp/debug.log
+screen -dm -S main bash -c "/var/web/main.sh"
+screen -dm -S loop2 bash -c "/var/web/loop2.sh"
+screen -dm -S blocks bash -c "/var/web/blocks.sh"
+screen -dm -S debug bash -c "tail -f /var/log/yiimp/debug.log"
